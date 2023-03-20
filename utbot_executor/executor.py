cd1ddb047e0851430cc9c207adf91e6b9b94d7bc
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2023-03-20 13:18:21 +0300

    0.200.10

--- utbot_executor/executor.py
+++ utbot_executor/executor.py
@@ -21,7 +21,7 @@ class PythonExecutor:
     def add_syspaths(self, syspaths: Iterable[str]):
         for path in syspaths:
             if path not in sys.path:
-                sys.path.append(path)
+                sys.path.insert(0, path)
 
     def add_imports(self, imports: Iterable[str]):
         for module in imports:

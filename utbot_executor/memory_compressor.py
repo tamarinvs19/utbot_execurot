1c85c23713124a3ba52e45b7c1d3bcf18e56ea38
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2023-04-10 17:44:18 +0300

    1.5.3: fix numpy comparator
    (cherry picked from commit d6d8ff795c283793f0b74a4debefe91cba160dd6)

--- utbot_executor/memory_compressor.py
+++ utbot_executor/memory_compressor.py
@@ -4,6 +4,16 @@ from utbot_executor.deep_serialization.memory_objects import MemoryDump
 from utbot_executor.deep_serialization.utils import PythonId
 
 
+def comparator(left: object, right: object) -> bool:
+    if type(left).__module__ == 'numpy':
+        if type(left).__qualname__ == 'ndarray':
+            return bool((left == right).all())
+        else:
+            return (left == right).__bool__()
+    else:
+        return left == right
+
+
 def compress_memory(
         ids: typing.List[PythonId],
         state_before: MemoryDump,
@@ -12,6 +22,6 @@ def compress_memory(
     diff_ids: typing.List[PythonId] = []
     for id_ in ids:
         if id_ in state_before.objects and id_ in state_after.objects:
-            if state_before.objects[id_].obj != state_after.objects[id_].obj:
+            if not comparator(state_before.objects[id_].obj, state_after.objects[id_].obj):
                 diff_ids.append(id_)
     return diff_ids

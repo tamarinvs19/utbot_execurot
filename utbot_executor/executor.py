8db8e4f460525d32e37a1f78911fa041b96220c0
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2023-04-27 14:05:22 +0300

    1.4.26: fix str to bytes

--- utbot_executor/executor.py
+++ utbot_executor/executor.py
@@ -111,7 +111,8 @@ class PythonExecutor:
             def _coverage_sender(info: typing.Tuple[str, int]):
                 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 logging.debug("Coverage message: %s:%d", request.coverage_id, info[1])
-                sock.sendto(bytes(f'{request.coverage_id}:{info[1]}'), (self.coverage_hostname, self.coverage_port))
+                message = bytes(f'{request.coverage_id}:{info[1]}', encoding='utf-8')
+                sock.sendto(message, (self.coverage_hostname, self.coverage_port))
                 logging.debug("ID: %s, Coverage: %s", request.coverage_id, info)
 
             value = _run_calculate_function_value(
